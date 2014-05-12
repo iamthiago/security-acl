171f8ca49f610ba25f939046e3f578bd18dc7902
Thiago <thiagoandrade6@gmail.com>
2014-05-12 13:35:54 -0300

    javadoc

--- src/main/java/com/security/service/AclManager.java
+++ src/main/java/com/security/service/AclManager.java
@@ -5,37 +5,48 @@ package com.security.service;
 
 import java.io.Serializable;
 
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.GrantedAuthoritySid;
+import org.springframework.security.acls.domain.PrincipalSid;
 import org.springframework.security.acls.model.Permission;
 import org.springframework.security.acls.model.Sid;
 
 /**
  * @author Thiago
+ * 
+ * Simple interface that manages ACL Permissions
  *
  */
 public interface AclManager {
 	
 	/**
-	 * @param clazz
-	 * @param identifier
-	 * @param sid
-	 * @param permission
+	 * Add a permission for the given object
+	 * 
+	 * @param clazz Domain class
+	 * @param identifier Id from the given domain
+	 * @param sid Security Identifier, could a {@link PrincipalSid} or a {@link GrantedAuthoritySid}
+	 * @param permission The permission based on {@link BasePermission}
 	 */
 	public <T> void addPermission(Class<T> clazz, Serializable identifier, Sid sid, Permission permission);
 	
 	/**
-	 * @param clazz
-	 * @param identifier
-	 * @param sid
-	 * @param permission
+	 * Remove a permission from the given object
+	 * 
+	 * @param clazz Domain class
+	 * @param identifier Id from the given domain
+	 * @param sid Security Identifier, could a {@link PrincipalSid} or a {@link GrantedAuthoritySid}
+	 * @param permission The permission based on {@link BasePermission}
 	 */
 	public <T> void removePermission(Class<T> clazz, Serializable identifier, Sid sid, Permission permission);
 	
 	/**
-	 * @param clazz
-	 * @param identifier
-	 * @param sid
-	 * @param permission
-	 * @return
+	 * Check whether the given object has permission
+	 * 
+	 * @param clazz Domain class
+	 * @param identifier Id from the given domain
+	 * @param sid Security Identifier, could a {@link PrincipalSid} or a {@link GrantedAuthoritySid}
+	 * @param permission The permission based on {@link BasePermission}
+	 * @return true or false
 	 */
 	public <T> boolean isPermissionGranted(Class<T> clazz, Serializable identifier, Sid sid, Permission permission);
 }
